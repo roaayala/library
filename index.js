6f17baf13a641201f3930dce26c6b790b61f0e95
roaayala <roaayala.ahm@gmail.com>
2026-03-17 22:37:06 +0800

    clean start and write book constuctor

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const myLibrary = [];
 
-function Book() {}
+function Book(title, author, pages, status) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.status = status;
+}
 
 function addBookToLibrary() {}
