5079b019aec29e3c3e4bd453ccce447fa4e7d5a8
roaayala <roaayala.ahm@gmail.com>
2026-04-14 23:25:46 +0800

    success open addBook modal

---
+++ main.js
@@ -0,0 +1,35 @@
+class Library {
+	constructor() {
+		this.dialogManager = new DialogManager();
+	}
+}
+
+class Book {
+	constructor(title, author, pages, status) {
+		this.id = crypto.randomUUID();
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.status = status;
+	}
+}
+
+class FormHandler {}
+
+class DialogManager {
+	constructor() {
+		this.addDialog = document.querySelector('#addBookDialog');
+		this.showAddDialog = document.querySelector('#showAddBookDialog');
+		this.editDialog = document.querySelector('#editBookDialog');
+
+		this.showAddDialog.addEventListener('click', () => {
+			this.addDialog['open']
+				? this.addDialog.close()
+				: this.addDialog.showModal();
+		});
+	}
+}
+
+class BookRenderer {}
+
+const app = new Library();
