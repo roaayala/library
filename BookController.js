15f10f3562720bdcdd03dbfebac5bac01c5416ea
roaayala <roaayala.ahm@gmail.com>
2026-05-24 12:17:01 +0800

    controller: BookController

---
+++ BookController.js
@@ -0,0 +1,24 @@
+import createBook from "./Book";
+
+const addBook = (libraryArray, dataObject) => {
+    const { title, author, pages, status } = dataObject;
+
+    const newBook = createBook(title, author, pages, status);
+
+    return [...libraryArray, newBook];
+};
+
+const removeBook = (libraryArray, id) => {
+    return libraryArray.filter((book) => book.id !== id);
+};
+
+const editBook = (libraryArray, id, dataObject) => {
+    return libraryArray.map((book) => {
+        if (book.id === id) {
+            return { ...book, ...dataObject };
+        }
+        return book;
+    });
+};
+
+export const BookController = { addBook, removeBook, editBook };
